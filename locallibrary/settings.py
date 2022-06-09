2caf2c2f4bf1d4b58edca10ed77654e3f95cc258
Abdulaziz <sukhrobjonov02@gmail.com>
2022-06-09 17:13:02 +0200

    changes settings, added static settings to settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise',
     'django.contrib.staticfiles',
     'catalog.apps.CatalogConfig',
 ]
@@ -136,7 +137,8 @@ DATABASES['default'].update(db_from_env)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
@@ -145,3 +147,4 @@ STATIC_URL = '/static/'
 # https://pypi.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
